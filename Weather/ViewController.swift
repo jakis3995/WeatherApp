a78596acee6f2f16b62f0d3105ea497480e10748
Maskim Klevtsov <max39.95@mail.ru>
2019-12-19 18:43:03 +0200

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Максим Клевцов  on 19/12/2019.
+//  Copyright © 2019 Максим Клевцов . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
